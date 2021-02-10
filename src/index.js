4a35e470f4dde4dad04c1e208be0bae813932c4f
Julian Angel <julian.arango@sourcecol.com>
2021-02-09 20:16:10 -0500

    Bootcamp part 3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = (props) => {
+  return <h1>{props.contadorInicial}</h1>;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let contador = 0;
+
+const refresh = () => {
+  ReactDOM.render(
+    <App contadorInicial={contador} />,
+    document.getElementById("root")
+  );
+};
+
+setInterval(() => {
+  refresh();
+  contador++;
+}, 1000);
